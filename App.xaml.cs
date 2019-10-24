0316f66734b9660d7f28c30b91ea66df92a36345
Ivan Matkov <v-ivmatk@microsoft.com>
2019-10-24 15:39:59 +0300

    Add App Center 2.4.0-preview

--- App.xaml.cs
+++ App.xaml.cs
@@ -6,6 +6,10 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+
 namespace WpfAppTest
 {
     /// <summary>
@@ -13,5 +17,11 @@ namespace WpfAppTest
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            AppCenter.LogLevel = LogLevel.Verbose;
+            AppCenter.Start("5a5c7382-f51d-4ab4-a67e-c41417d34365",
+                   typeof(Analytics), typeof(Crashes));
+        }
     }
 }
